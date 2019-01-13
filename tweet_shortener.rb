5de02cbe9c126c8a4cb966bc60c14294f3dc0dca
Shemar Dacosta <dshemar6@gmail.com>
2019-01-13 15:14:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+# Write your code help
+def dictionary 
+  {
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2', 
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
+}
+end
+
+def word_substituter(tweet)
+temp = tweet.split
+
+dictionary.each do |key,value|
+  temp.each do |word|
+   if key.to_s == word
+     word.replace(value)
+   end
+  end
+ end
+temp.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+tweets.each do |tweet|
+puts  word_substituter(tweet)
+
+end
+end
